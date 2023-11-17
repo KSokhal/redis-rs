d2bbfcf71714704d169397b1a069534f171003bf
KSokhal <kulvir.sokhal@hotmail.com>
2023-11-17 19:46:27 +0000

    Added parsing of RESP inputs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
-use std::io::{BufRead, BufReader, Read, Write};
-use std::net::{TcpListener, TcpStream};
+use anyhow::Result;
+use std::io::{BufReader, Write};
+use std::net::TcpListener;
 
-fn main() -> std::io::Result<()> {
+use crate::parser::RespReader;
+
+mod parser;
+
+fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379")?;
 
-    // accept connections and process them serially
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 println!("New connection: {}", stream.peer_addr().unwrap());
 
-                let mut reader = BufReader::new(&mut stream);
+                let reader = BufReader::new(&mut stream);
+                let mut parser = RespReader::new(reader);
 
-                let mut e = String::new();
-                reader.read_line(&mut e);
-                println!("{:?}", e);
+                let value = parser.parse()?;
+
+                println!("{:#?}", value);
 
                 stream.write_all(b"+PONG\r\n")?;
             }
---
+++ src/parser.rs
@@ -0,0 +1,118 @@
+use anyhow::Error;
+use std::io::{BufReader, Read};
+
+const STRING: char = '+';
+const ERROR: char = '-';
+const INTEGER: char = ':';
+const BULK: char = '$';
+const ARRAY: char = '*';
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Value {
+    _type: char,
+    str: Option<String>,
+    num: Option<i32>,
+    bulk: Option<String>,
+    array: Option<Vec<Value>>,
+}
+
+impl Value {
+    fn new(_type: char) -> Self {
+        Self {
+            _type,
+            str: None,
+            num: None,
+            bulk: None,
+            array: None,
+        }
+    }
+}
+
+pub struct RespReader<T: std::io::Read> {
+    reader: BufReader<T>,
+}
+
+impl<T: std::io::Read> RespReader<T> {
+    pub fn new(reader: BufReader<T>) -> Self {
+        Self { reader }
+    }
+
+    fn read_char(&mut self) -> Result<char, Error> {
+        let mut char: [u8; 1] = [0; 1];
+        self.reader.read_exact(&mut char)?;
+        Ok(char[0] as char)
+    }
+
+    fn read_int(&mut self) -> Result<usize, Error> {
+        let mut num: [u8; 1] = [0; 1];
+        self.reader.read_exact(&mut num)?;
+        let n = (num[0] as char)
+            .to_digit(10)
+            .expect("Failed to read int from buffer");
+        Ok(n as usize)
+    }
+
+    fn read_str(&mut self, size: usize) -> Result<String, Error> {
+        let mut buf = vec![0; size];
+        self.reader.read_exact(&mut buf)?;
+        Ok(String::from_utf8_lossy(&buf).to_string())
+    }
+
+    fn consume_newline(&mut self) -> Result<(), Error> {
+        self.reader.read_exact(&mut [0; 2])?;
+        Ok(())
+    }
+
+    pub fn parse(&mut self) -> Result<Value, Error> {
+        let _type = self.read_char()?;
+
+        match _type {
+            ARRAY => {
+                let mut value = Value::new(ARRAY);
+
+                let arr_len = self.read_int()?;
+
+                let mut value_arr: Vec<Value> = Vec::new();
+
+                for _ in 0..arr_len {
+                    self.consume_newline()?;
+                    let v = self.parse()?;
+                    value_arr.push(v);
+                }
+                value.array = Some(value_arr);
+                Ok(value)
+            }
+            BULK => {
+                let mut value = Value::new(BULK);
+
+                let str_len = self.read_int()?;
+                self.consume_newline()?;
+
+                let string = self.read_str(str_len)?;
+
+                value.bulk = Some(string);
+
+                Ok(value)
+            }
+            _ => panic!("unknown type {}", _type),
+        }
+    }
+}
+
+#[test]
+fn parse_resp() {
+    let input = "*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n";
+
+    let mut parser = RespReader::new(BufReader::new(input.as_bytes()));
+
+    let mut hello = Value::new(BULK);
+    hello.bulk = Some("hello".to_string());
+
+    let mut world = Value::new(BULK);
+    world.bulk = Some("world".to_string());
+
+    let mut expected_value = Value::new(ARRAY);
+    expected_value.array = Some(vec![hello, world]);
+
+    assert!(parser.parse().unwrap() == expected_value);
+}
