6aa25b66158ab4900497e19bee483973a1cb6611
KSokhal <kulvir.sokhal@hotmail.com>
2023-11-19 14:15:32 +0000

    Restructured to use datastore struct Addded hset, hget, hgetall

--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,114 @@
-use crate::{parser::Value, Db};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
-fn ping(x: Vec<Value>, _: Db) -> Value {
+use crate::parser::Value;
+
+// HashMap that be shared between threads
+// Mutex is used to ensure that only one thread can access the HashMap at a time, by locking and unlocking the value
+// Arc is used to allow the HashMap is shared between threads
+// Atomic reference counting (Arc) counts the number of reference that point to the same memory heap
+// It is thread-safe since is uses atomic operations to increment and decrement the reference count, however they are more expensive
+// Not currently needed, since only one thread, but will be useful when we add concurrency
+
+pub struct Datastore {
+    db: Mutex<HashMap<String, String>>,
+    hash_db: Mutex<HashMap<String, HashMap<String, String>>>,
+}
+
+impl Datastore {
+    pub fn new() -> Self {
+        Self {
+            db: Mutex::new(HashMap::new()),
+            hash_db: Mutex::new(HashMap::new()),
+        }
+    }
+
+    pub fn set(&self, values: Vec<Value>) -> Value {
+        if values.len() != 2 {
+            return Value::Error("Wrong number of arguments".to_string());
+        }
+        let mut db = self.db.lock().unwrap();
+        db.insert(values[0].unwrap_str(), values[1].unwrap_str());
+
+        Value::Str("OK".to_string())
+    }
+
+    pub fn get(&self, values: Vec<Value>) -> Value {
+        if values.len() != 1 {
+            return Value::Error("Wrong number of arguments".to_string());
+        }
+
+        let db = self.db.lock().unwrap();
+
+        match db.get(&values[0].unwrap_str()) {
+            Some(value) => Value::Bulk(value.to_string()),
+            None => Value::Null,
+        }
+    }
+
+    pub fn hset(&self, values: Vec<Value>) -> Value {
+        if values.len() != 3 {
+            return Value::Error("Wrong number of arguments".to_string());
+        }
+        let mut hash_db = self.hash_db.lock().unwrap();
+        let key = values[0].unwrap_str();
+        let field = values[1].unwrap_str();
+        let value = values[2].unwrap_str();
+
+        match hash_db.get_mut(&key) {
+            Some(map) => {
+                map.insert(field.to_string(), value.to_string());
+            }
+            None => {
+                let mut map = HashMap::new();
+                map.insert(field.to_string(), value.to_string());
+                hash_db.insert(key.to_string(), map);
+            }
+        }
+        Value::Str("OK".to_string())
+    }
+
+    pub fn hget(&self, values: Vec<Value>) -> Value {
+        if values.len() != 2 {
+            return Value::Error("Wrong number of arguments".to_string());
+        }
+        let hash_db = self.hash_db.lock().unwrap();
+        let key = values[0].unwrap_str();
+        let field = values[1].unwrap_str();
+
+        match hash_db.get(&key) {
+            Some(map) => match map.get(&field) {
+                Some(value) => Value::Bulk(value.to_string()),
+                None => Value::Null,
+            },
+            None => Value::Null,
+        }
+    }
+
+    pub fn hgetall(&self, values: Vec<Value>) -> Value {
+        if values.len() != 1 {
+            return Value::Error("Wrong number of arguments".to_string());
+        }
+        let hash_db = self.hash_db.lock().unwrap();
+        let key = values[0].unwrap_str();
+
+        match hash_db.get(&key) {
+            Some(map) => {
+                let mut resp = vec![];
+                for (key, value) in map {
+                    resp.push(Value::Bulk(key.to_string()));
+                    resp.push(Value::Bulk(value.to_string()));
+                }
+                return Value::Array(resp);
+            }
+            None => Value::Null,
+        }
+    }
+}
+
+fn ping(x: Vec<Value>) -> Value {
     if x.is_empty() {
         Value::Str("PONG".to_string())
     } else {
@@ -8,42 +116,20 @@ fn ping(x: Vec<Value>, _: Db) -> Value {
     }
 }
 
-fn unknown(x: Vec<Value>, _: Db) -> Value {
-    Value::Error(format!("Unknown command: {}", x[0].unwrap_str()))
-}
+pub fn handler(values_arr: Vec<Value>, datastore: &Arc<Datastore>) -> Value {
+    let (first, values) = values_arr.split_first().unwrap();
 
-fn set(x: Vec<Value>, db: Db) -> Value {
-    if x.len() == 2 {
-        let mut db = db.lock().unwrap();
-        db.insert(x[0].unwrap_str(), x[1].unwrap_str());
-        Value::Str("OK".to_string())
-    } else {
-        Value::Error("Wrong number of arguments".to_string())
-    }
-}
-
-fn get(x: Vec<Value>, db: Db) -> Value {
-    if x.len() == 1 {
-        let db = db.lock().unwrap();
-        match db.get(&x[0].unwrap_str()) {
-            Some(value) => Value::Bulk(value.to_string()),
-            None => Value::Null,
-        }
-    } else {
-        Value::Error("Wrong number of arguments".to_string())
-    }
-}
-
-pub fn handler(value: &Value) -> impl Fn(Vec<Value>, Db) -> Value {
-    // println!("{:#?}", value);
-    if let Value::Bulk(command) = value {
+    if let Value::Bulk(command) = first {
         match command.to_lowercase().as_str() {
-            "ping" => ping,
-            "set" => set,
-            "get" => get,
-            _ => unknown,
+            "ping" => ping(values.to_vec()),
+            "set" => datastore.set(values.to_vec()),
+            "get" => datastore.get(values.to_vec()),
+            "hset" => datastore.hset(values.to_vec()),
+            "hget" => datastore.hget(values.to_vec()),
+            "hgetall" => datastore.hgetall(values.to_vec()),
+            _ => Value::Error(format!("Unknown command: {}", command)),
         }
     } else {
-        unknown
+        Value::Error("Unknown value type".to_string())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use anyhow::Result;
-use std::collections::HashMap;
 use std::net::TcpListener;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
-use crate::handler::handler;
+use crate::handler::{handler, Datastore};
 use crate::parser::RespReader;
 use crate::writer::RespWriter;
 
@@ -11,23 +10,14 @@ mod handler;
 mod parser;
 mod writer;
 
-type Db = Arc<Mutex<HashMap<String, String>>>;
-
 fn main() -> Result<()> {
-    // HashMap that be shared between threads
-    // Mutex is used to ensure that only one thread can access the HashMap at a time, by locking and unlocking the value
-    // Arc is used to allow the HashMap is shared between threads
-    // Atomic reference counting (Arc) counts the number of reference that point to the same memory heap
-    // It is thread-safe since is uses atomic operations to increment and decrement the reference count, however they are more expensive
-    let db: Db = Arc::new(Mutex::new(HashMap::new()));
-
     let listener = TcpListener::bind("127.0.0.1:6379")?;
+
+    let datastore = Arc::new(Datastore::new());
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-
                 // let reader = BufReader::new(&mut stream);
                 let mut parser = RespReader::new(&mut stream);
 
@@ -35,7 +25,7 @@ fn main() -> Result<()> {
 
                 let values_arr = value.unwrap_arr();
 
-                let response = handler(&values_arr[0])(values_arr[1..].to_vec(), db.clone());
+                let response = handler(values_arr, &datastore);
 
                 let mut writer = RespWriter::new(stream);
                 writer.write(response)?;
